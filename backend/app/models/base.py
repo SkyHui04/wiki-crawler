1d7a070a4165b9df54055ddb77c0e9117570c626
skyhui <skyhui04@gmail.com>
2026-09-22 21:06:08 +0800

    Implement get_indexed_fields with computed fields, move repeated methods to a shared class.

--- backend/app/models/base.py
+++ backend/app/models/base.py
@@ -1,7 +1,10 @@
 from pydantic import BaseModel
 from typing import Literal, Annotated
+from abc import ABC
 
 type LatticeIndexType = Literal["Exact", "FTS"]
+
+_LATTICE_INDEX_TYPES: tuple[LatticeIndexType, ...] = ("Exact", "FTS")
 
 
 class LatticeIndex:
@@ -11,7 +14,58 @@ class LatticeIndex:
         self.index_type = index_type
 
 
-class BaseNode(BaseModel):
+class LatticeIndexableModel(ABC, BaseModel):
+    """
+    Fields in LatticeIndexableModel can be automatically extracted for
+    indexing in Lattice Database by the following methods: \n
+    1.  Annotate the field with :class:`~app.models.base.LatticeIndex` using :class:`~typing.Annotated`.
+    This does not work for computed fields.
+    ```python
+    indexed_field: Annotated[str, LatticeIndex("Exact")]
+    ```
+    2.  Add `"lattice_index"` to `json_schema_extra`.
+    ```python
+    @computed_field(
+        json_schema_extra={"lattice_index": "Exact"},
+    )
+    @property
+    def indexed_computed_field(self) -> str:
+        ...
+    ```
+    """
+
+    @classmethod
+    def get_indexed_fields(cls) -> dict[str, LatticeIndexType]:
+        indexed_properties: dict[str, LatticeIndexType] = {}
+
+        for field_name, field_info in cls.model_fields.items():
+            for metadata in field_info.metadata:
+                if isinstance(metadata, LatticeIndex):
+                    indexed_properties[field_name] = metadata.index_type
+
+        for field_name, field_info in (
+            cls.model_fields | cls.model_computed_fields
+        ).items():
+            if field_info.json_schema_extra is not None and not callable(
+                field_info.json_schema_extra
+            ):
+                field_properties = field_info.json_schema_extra
+                if "lattice_index" not in field_properties:
+                    continue
+
+                if field_properties["lattice_index"] in _LATTICE_INDEX_TYPES:
+                    indexed_properties[field_name] = field_properties["lattice_index"]
+                else:
+                    raise ValueError(
+                        f"The lattice_index property of field {field_name} must take "
+                        f"the following values: {_LATTICE_INDEX_TYPES}\n"
+                        f"Instead, this is found: {field_properties["lattice_index"]}"
+                    )
+
+        return indexed_properties
+
+
+class BaseNode(LatticeIndexableModel):
     id: Annotated[str, LatticeIndex("Exact")]
 
     @classmethod
@@ -26,21 +80,8 @@ class BaseNode(BaseModel):
     def get_fields(cls) -> list[str]:
         return list(cls.model_fields.keys()) + list(cls.model_computed_fields.keys())
 
-    @classmethod
-    def get_indexed_fields(cls) -> dict[str, LatticeIndexType]:
-        indexed_properties = {}
 
-        for field_name, field_info in cls.model_fields.items():
-            for metadata in field_info.metadata:
-                if isinstance(metadata, LatticeIndex):
-                    indexed_properties[field_name] = metadata.index_type
-
-        # TODO: index computed fields
-
-        return indexed_properties
-
-
-class BaseEdge(BaseModel):
+class BaseEdge(LatticeIndexableModel):
     id: Annotated[str, LatticeIndex("Exact")]
 
     @classmethod
@@ -50,14 +91,3 @@ class BaseEdge(BaseModel):
     @classmethod
     def get_fields(cls) -> list[str]:
         return list(cls.model_fields.keys()) + list(cls.model_computed_fields.keys())
-
-    @classmethod
-    def get_indexed_fields(cls) -> dict[str, LatticeIndexType]:
-        indexed_properties = {}
-
-        for field_name, field_info in cls.model_fields.items():
-            for metadata in field_info.metadata:
-                if isinstance(metadata, LatticeIndex):
-                    indexed_properties[field_name] = metadata.index_type
-
-        return indexed_properties
